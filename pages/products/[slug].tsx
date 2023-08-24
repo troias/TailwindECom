da23aa65f42ac5b28acdf1d5b704e155ff91f058
troias <hunter007nz@gmail.com>
2023-08-24 17:51:46 +1200

    added rating query and component

--- pages/products/[slug].tsx
+++ pages/products/[slug].tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { format } from "date-fns";
 import { formatPrice } from "../../utils/utils";
+import { StarIcon } from "@heroicons/react/20/solid";
 
 import {
   GetStaticPaths,
@@ -17,6 +18,8 @@ function classNames(...classes) {
 
 export default function Example({ product, products }) {
   const [isLoading, setIsLoading] = useState(false);
+
+  console.log("product", product);
 
   const relatedProducts = products
     .filter((item) => item.node.handle !== product.handle)
@@ -83,11 +86,50 @@ export default function Example({ product, products }) {
                   {product.title}
                 </h1>
 
+                {/* Reviews */}
+                <div className="mt-4">
+                  <h2 className="sr-only">Reviews</h2>
+                  <div className="flex items-center">
+                    <p className="text-sm text-gray-700">
+                      {product.rating}
+                      <span className="sr-only"> out of 5 stars</span>
+                    </p>
+                    <div className="ml-1 flex items-center">
+                      {[0, 1, 2, 3, 4].map((rating) => (
+                        <StarIcon
+                          key={rating}
+                          className={classNames(
+                            product.rating > rating
+                              ? "text-yellow-400"
+                              : "text-gray-200",
+                            "h-5 w-5 flex-shrink-0"
+                          )}
+                          aria-hidden="true"
+                        />
+                      ))}
+                    </div>
+                    <div
+                      aria-hidden="true"
+                      className="ml-4 text-sm text-gray-300"
+                    >
+                      ·
+                    </div>
+                    <div className="ml-4 flex">
+                      <a
+                        href="#"
+                        className="text-sm font-medium text-indigo-600 hover:text-indigo-500"
+                      >
+                        See all {product.reviewCount} reviews
+                      </a>
+                    </div>
+                  </div>
+                </div>
+
                 <h2 id="information-heading" className="sr-only">
                   Product information
                 </h2>
                 <p className="mt-2 text-sm text-gray-500">
-                  Version {product.tags[0]} &middot; (Updated{" "}
+                  (Updated{" "}
                   <time
                     dateTime={format(new Date(product.updatedAt), "dd MM yyyy")}
                   >
@@ -280,8 +322,15 @@ export const getStaticProps: GetStaticProps = async (
 
   const singleProduct = await graphqlstorefront(
     gql`
-      query ProductByHandle($handle: String!) {
+      query ProductByHandle(
+        $handle: String!
+        $namespace: String!
+        $key: String!
+      ) {
         product(handle: $handle) {
+          metafield(namespace: $namespace, key: $key) {
+            value
+          }
           handle
           id
           title
@@ -334,7 +383,7 @@ export const getStaticProps: GetStaticProps = async (
         }
       }
     `,
-    { handle }
+    { handle, namespace: "ratings", key: "ratings" }
   );
 
   const product = singleProduct.product;
